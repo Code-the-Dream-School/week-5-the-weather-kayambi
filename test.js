329b6a976bbfe123006d4dea9afedee932add658
kayambi <you@example.com>
2020-07-08 23:48:52 -0700

    added city and country foracast

--- test.js
+++ test.js
@@ -1,145 +1,64 @@
+const api = {
+    key:"7432c602e487c2cf5ba43b3f4ea7e8d6", 
+    base:"https://api.openweathermap.org/data/2.5/",
+    // baseforCast ="https://api.openweathermap.org/data/2.5/onecall?exclude=minutely,hourly&"
 
+  }
+// test the eventListiner
 
+const searchbox =document.querySelector(".search").
+searchbox.addEventListener('input', myFunction);
 
-  // another option that I would consider
+function myFunction(e) {
+    getResult(searchbox.value);
+}
+
+//   BUiling weather forcast
+
+  function getResult(query) {
+    fetch(`${api.base}weather?q=${query}&units=metrics&appid=${api.key}`)
+          .then(weather => {
+            return weather.json();
   
-  // function weatherData () {
-  //      let key= '{key goes here}'
-  //      fetch(`https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=7432c602e487c2cf5ba43b3f4ea7e8d6`)
-  //      .then(function(response){ return response.json() })  // convert to json 
-  //     .then(function(data){
-  //            getWeather(data);  // calling weather to my page   
-  //      })
-  //      .catch(function(){
-  //        // display that the weather data do go eat them lol , then error message
-  //      });
-  // } 
-  //  window.onload = function () {
-  //    weatherData();
-  //  }
-
-  // function helper   
-
-  // This function will take the weather data as a parameter and insert it into 
-  // the description,temp and location div elements.
-
-  // function getWeather (d) {
-  //   var celcius = Math.round(parseFloat(d.main.temp) -273.15);
-  //   var fahreinheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
-  //   var description = d.weather[0].description;
-
-  //   document.getElementById('description').innerHTML=d.weather[0].description;
-  //   document.getElementById('temp').innerHTML=celcius + '&deg';
-  //   document.getElementById('location').innerHTML=d.name;  
-
-  //   if (description.indexOf('rain') > 0) {
-  //     document.body.className ='rainy'
-        
-  //     }else if (description.indexOf('cloud') > 0 ) {
-  //       document.body.className = 'cloudy';
-  //     }else if (description.indexOf('sunny') > 0 ){
-  //       document.body.className = 'sunny';
-  //     }else {
-  //       document.body.className='clear';
-  //     }
-  //   }
-  //   window.onload = function() {
-  //     weatherData();
-  //   } 
-    // Adding Event listiner, getting data into EventListiner using object. 
+          }).then (displayResults);
+  
+  }
+//    the function to display result to UI 
+  
+  function displayResults(weather){
+  
+    // console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerHTML = `${weather.name} ${weather.sys.country}`;
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText =dateBuilder(now); 
     
-    // searchBtn.addEventListener('click',  
-
-
-
-
-
-
-// //  creating fetch function 
-
-// feth(`https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=7432c602e487c2cf5ba43b3f4ea7e8d6`) 
-//      .then(res => res.json()) 
-//      .the(data => console.log(data))
-
-
-
-// //  Creating a reusable function  
-
-// function fetchData(url){
-//     return fetch(url)
-//            .then(checkStatus)
-//            .then(res => res.json())
-//            .catch(error => console.log ('looks like the dog eaat your site', error))
-// }
-
-//     // Fetch All weather for 7 days and  it's geographic coordinate
-//   Promise.all([
-//   fetchData(`https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=7432c602e487c2cf5ba43b3f4ea7e8d6`),
-//   fetchData(`https://api.openweathermap.org/data/2.5/onecall?lat=37.77&lon=-122.42&exclude=minutely,hourly&appid=7432c602e487c2cf5ba43b3f4ea7e8d6`)
-
-//   ])
-//   .then(data => {
-
-//     const weatherCoords = data[0].message;
-//     const sevenDaysCall = data[1].message;
-
-//     generateGeoCoords(weatherCoords);
-//     generateSevenDaysData(sevenDaysCall);
-   
-//   })
-
-//   //  Helper Functions 
-//   //  checking the status 
-
-//   function checkStatus (response){
-//     if (response.ok) {
-//       return Promise.resolve (response);
-
-//     } else {
-//     return Promise.reject(new Error(response.statustext));
-//   }
-//   }
-
-
-//     //  function to generate data for geographical coordinate
-
-//     function generateGeoCoords (data) { 
-
-//       const geocords = `<input src='${data}' >
-//       <p>I want my in put to be here of 
-      
-//       `;
-
-
-
-//     }
-
-//   //  function generatedata for seven days  to the html
-  
-//   function generateSevenDaysData (data) {
-
-//   }
-
-
-//   //  Event Listners  
-
-
+    let temp = document.querySelector('.current .temp') 
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>•c</span>`;
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
   
 
+  }
+//   set the date bulder function
+
+  function dateBuilder(d) { 
+
+    let months =["January","February","March","April","May",
+    "June","July","August","September","October","November","December"];
+
+    let days= ["sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month =months [d.getMonth()]
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+
+  }
+
+// This will be  the process of getting Data
 
 
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-  
--- weather.js
+++ weather.js
@@ -1,72 +1,65 @@
-// my intitialization
-const searchBtn = document.getElementById('search')
-const   mycity = document.getElementById('city')
-const tableInfo = document.getElementById('info')
+// // my intitialization
+// const searchBtn = document.getElementById('search');
+// const   mycity = document.getElementById('city');
+// const tableInfo = document.getElementById('info');
+// const mycity= document.querySelector(".cityName p");
+// const myCountry= document.querySelector(".countryName p ");
+// const myWeather= document.querySelector(".currentWeather p");
 
-const sevenDayHost = "https://api.openweathermap.org/data/2.5/onecall?exclude=minutely,hourly&";
-const weatherHost = "https://api.openweathermap.org/data/2.5/weather?q=";
-const authLock = "appid=7432c602e487c2cf5ba43b3f4ea7e8d6";
+// const sevenDayHost = "https://api.openweathermap.org/data/2.5/onecall?exclude=minutely,hourly&";
+// const weatherHost = "https://api.openweathermap.org/data/2.5/weather?q=";
+// const authLock = "appid=7432c602e487c2cf5ba43b3f4ea7e8d6";
 
-var city = "Oakland"; //input.value;
-var lat, lon, forecastData;
+// var city = "San Francisco"; //input.value;
+// var lat, lon, forecastData;
 
-// Build weather data url for api call to get lat and lon
-function weatherURL() {
-  return `${weatherHost}${city}&${authLock}`;
-}
+// // Build weather data url for api call to get lat and lon
+// function weatherURL() {
+//   return `${weatherHost}${city}&${authLock}`;
+// }
 
-// Build forecast data url for api call to get seven forecast info
-function forecastURL() {
-  return `${sevenDayHost}&lat=${lat}&lon=${lon}&${authLock}`;
-}
+// // Build forecast data url for api call to get seven forecast info
+// function forecastURL() {
+//   return `${sevenDayHost}&lat=${lat}&lon=${lon}&${authLock}`;
+// }
+// // 
+// // Global fetch function that creates a promise to return data.
+// async function getData(dataURL) {
+//   let response = await fetch(dataURL);
+//   return response.json();
+// }
 
-// Global fetch function that creates a promise to return data.
-async function getData(dataURL) {
-  let response = await fetch(dataURL);
-  return response.json();
-}
+// // Returns seven day forecast data
+// function getForecastData(url) {
+//   return getData(url).then(data => {
+//     // Then set the forecastData variable to the data received
+//     forecastData = data;
+//   });
+// }
 
-// Returns seven day forecast data
-function getForecastData(url) {
-  return getData(url).then(data => {
-    // Then set the forecastData variable to the data received
-    forecastData = data;
-  });
-}
+// // Returns weather data for lat and lon
+// function getAllData(url) {
+//   // first get lat and lon from weather api
+//   getData(url)
+//     .then(data => {
+//       // Once data is received, update global vars {lat and lon}
+//       lat = data.coord.lat;
+//       lon = data.coord.lon;
+//     })
+//     .then(() => {
+//       // Once the lat and lon have been update, Call/Invoke the function to get the seven day forecast data
+//       getForecastData(forecastURL());
+//     })
 
-// Returns weather data for lat and lon
-function getAllData(url) {
-  // first get lat and lon from weather api
-  getData(url)
-    .then(data => {
-      // Once data is received, update global vars {lat and lon}
-      lat = data.coord.lat;
-      lon = data.coord.lon;
-    })
-    .then(() => {
-      // Once the lat and lon have been update, Call/Invoke the function to get the seven day forecast data
-      getForecastData(forecastURL());
-    })
-    .then(() => {
-      document.getElementById("city").innerHTML = forecastData;
-    });
+// //     .then(() => {
 
-}
+// //     searchBtn.addEventListener('click',function(){
+// //         mycity.innerHTML = forecastData;
 
-(getAllData(weatherURL()));  
+// //       })
 
-//  I need  to  map my data into html but I am still working on it. 
+// //     });
 
+// // }
 
-
-
-
-
-
-
-
-
-
-
-
-
+// // (getAllData(weatherURL()));
